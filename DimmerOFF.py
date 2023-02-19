fc9794d23654d08b5ac57c7c7477b00ec16cb923
CORDIALCHIP <larj2003@gmail.com>
2023-02-20 00:32:54 +0300

    NewScript

--- DimmerOFF.py
+++ DimmerOFF.py
@@ -6,7 +6,7 @@ from threading import Timer
 brightness = sbc.get_brightness()# get brightness of monitor
 global primary
 primary = sbc.get_brightness(display=0)# get brightness of prim. monitor
-sbc.set_brightness(2)
+sbc.set_brightness(65)
 for monitor in sbc.list_monitors():
     print(monitor, ':', sbc.get_brightness(display=monitor), '%')
 #for .exe format, if you need use pyinstaller -F (file_name);
