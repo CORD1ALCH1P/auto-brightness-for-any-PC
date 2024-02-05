a2df4f50d39dc9d8d43db8c265b2252e3fc10292
CORDIALCHIP <98459987+CORD1ALCH1P@users.noreply.github.com>
2024-02-06 00:46:42 +0300

    Delete Dimmer.py

--- Dimmer.py
+++
@@ -1,9 +0,0 @@
-import screen_brightness_control as sbc
-
-brightness = sbc.get_brightness()# get brightness of monitor
-global primary
-primary = sbc.get_brightness(display=0)# get brightness of prim. monitor
-sbc.set_brightness(2)
-for monitor in sbc.list_monitors():
-    print(monitor, ':', sbc.get_brightness(display=monitor), '%')
-#for .exe format, if you need use pyinstaller -F (file_name);
